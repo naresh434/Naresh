474d810e9a8c52c5d23118c4bba458e9f405b972
Naregadu <naresh.gorantla@fissionlabs.in>
2013-12-30 19:54:10 +0530

    main.java1 is changed

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		MyMath math = new MyMath();
 		System.out.println("Result is: " + math.multi(5, 10));
-		System.out.println("Result is: " + math.multi(5, 10));
+		System.out.println("Result is: " + math.multi(5, 100));
+		
 	}
 
 }
